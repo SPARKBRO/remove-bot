87cda515d7e309f5f1df67d1b9551f83bf29f494
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2023-02-28 13:08:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    API_ID = os.environ.get('API_ID')
+    API_HASH = os.environ.get('API_HASH')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
