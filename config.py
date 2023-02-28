a1320e8bd1700ce30bb150f27787bb10a78461f6
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2023-02-28 13:10:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-class Config:
-    API_ID = os.environ.get('API_ID')
+
+class Config(object):
+    API_ID = int(os.environ.get('API_ID'))
     API_HASH = os.environ.get('API_HASH')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
+    MONGO_URI = os.environ.get('MONGO_URI')
